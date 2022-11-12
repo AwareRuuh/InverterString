44ddb5343c277059b0ae57687cd01bdce4392770
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-11-12 17:26:09 +0000

    finalizado

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner sc1 = new Scanner(System.in);
+    System.out.println("Digite uma frase: ");
+    String fraseUsuario = sc1.nextLine();
+    String[] fraseInvertida = fraseUsuario.split(" ");
+    for (int i = fraseInvertida.length-1; i >= 0; i--){
+      System.out.print(fraseInvertida[i] + " ");
+    }
   }
 }
